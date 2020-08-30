83bdfa3cb28a90f58c9344c3acd10f16ccd749fa
Metro Phobe <metrophobe@FALCO-IMAC.local>
2020-08-30 15:31:13 +0200

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//to build and run ... use 'cargo run'
+//to build only  ... use 'cargo build' 
+//to build for production .... use 'cargo build --release'
+
